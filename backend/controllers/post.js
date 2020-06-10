faf7b7ae87a664b704f7a0869ceb5e0ac7e851ad
Ronald Allan Patawaran <ronaldallanpatawaran@gmail.com>
2020-06-10 15:47:38 +0800

    Added Post Module, bug fixed on the backend.

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -93,7 +93,7 @@ exports.updatePost = (req, res)=> {
     )
     Post.updateOne({_id: req.body.id, creator: req.userData.userId}, post)
       .then((result)=> {
-        if (result.nModified > 0) {
+        if (result.n > 0) {
           res.status(200)
           .json({
             message: 'Post successfully updated!',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
-import { ReactiveFormsModule, FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { HTTP_INTERCEPTORS } from '@angular/common/http'
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { PostCreateComponent } from './posts/post-create/post-create.component'
 import { HeaderComponent } from './header/header.component'
-import { PostListComponent } from './posts/post-list/post-list.component'
 import { LoginComponent } from './auth/login/login.component'
 import { SignupComponent } from './auth/signup/signup.component'
 import { ErrorComponent } from './error/error.component'
@@ -16,24 +14,24 @@ import { ErrorComponent } from './error/error.component'
 import { AuthInterceptor } from './auth/auth-interceptor'
 import { ErrorInterceptor } from './error-interceptor'
 import { AngularMaterialModule } from './angular-material.module'
+import { PostModule } from './posts/post.module'
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    PostCreateComponent,
     HeaderComponent,
-    PostListComponent,
     SignupComponent,
     ErrorComponent
   ],
   imports: [
+    AngularMaterialModule,
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
     FormsModule,
     BrowserAnimationsModule,
-    AngularMaterialModule
+    HttpClientModule,
+    PostModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
