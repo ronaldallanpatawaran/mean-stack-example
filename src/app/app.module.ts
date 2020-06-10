614972750c0c009311aa5b6b0b6f8d8034d4a61d
Ronald Allan Patawaran <ronaldallanpatawaran@gmail.com>
2020-06-10 14:32:14 +0800

    Added angular material module to separate angular material imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,7 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { ReactiveFormsModule, FormsModule } from '@angular/forms'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { MatInputModule } from '@angular/material/input'
-import { MatCardModule } from '@angular/material/card'
-import { MatButtonModule } from '@angular/material/button'
-import { MatToolbarModule } from '@angular/material/toolbar'
-import { MatExpansionModule } from '@angular/material/expansion'
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
-import { MatPaginatorModule } from '@angular/material/paginator'
-import { MatDialogModule } from '@angular/material/dialog'
+import { HTTP_INTERCEPTORS } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
@@ -23,6 +15,7 @@ import { ErrorComponent } from './error/error.component'
 
 import { AuthInterceptor } from './auth/auth-interceptor'
 import { ErrorInterceptor } from './error-interceptor'
+import { AngularMaterialModule } from './angular-material.module'
 
 @NgModule({
   declarations: [
@@ -40,15 +33,7 @@ import { ErrorInterceptor } from './error-interceptor'
     ReactiveFormsModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatInputModule,
-    MatCardModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatExpansionModule,
-    HttpClientModule,
-    MatProgressSpinnerModule,
-    MatPaginatorModule,
-    MatDialogModule
+    AngularMaterialModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
